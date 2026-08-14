8a5b71307d2e087683ba01bf44cf849267e5532d
François M. <francoism90@users.noreply.github.com>
2026-08-14 16:18:27 +0200

    Update application aliases in alias.fish
    Removed the alias for 'claude' and updated 'vsc' to open in a new window.

--- .config/fish/conf.d/alias.fish
+++ .config/fish/conf.d/alias.fish
@@ -1,6 +1,4 @@
-# Agents
-abbr -a claude 'toolbox run --container claude-agent claude'
-
 # Applications
-abbr -a vsc 'flatpak run com.visualstudio.code .'
+abbr -a code 'flatpak run com.visualstudio.code --new-window .'
+abbr -a vsc 'flatpak run com.visualstudio.code --new-window .'
 abbr -a zed 'flatpak run dev.zed.Zed-Preview .'
